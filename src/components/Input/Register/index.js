861e690bd8dbbe6ed1e11790b8f79a474532f40c
GustavoBrPereira <gustavobritope@gmail.com>
2020-09-18 23:04:51 -0300

    Corrige erro da pasta do Input de cadastro

---
+++ src/components/Input/Register/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+const InputRegister = styled.TextInput`
+    font-family: Roboto;
+    font-size: 25px;
+    height: 35px;
+    text-align: left;
+    color: #000;
+    margin: 10px 20px;
+    border-bottom-color: #C0CCDA;
+    border-bottom-width: 2px;
+
+`;
+
+export default InputRegister;
